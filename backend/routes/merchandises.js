fed9b64e073ea5f2c1c13100a50a223f1112d163
yuhanj2 <jyhan043@gmail.com>
2020-09-10 11:45:20 +0800

    hiding .env file

--- backend/routes/merchandises.js
+++ backend/routes/merchandises.js
@@ -43,4 +43,39 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Database add new Merchandise Error: ' + err));
 });
 
+// fetch a specific merchandise by document ID
+router.route('/:id').get((req, res) => {
+    Merchandise.findById(req.params.id)
+        .then(merchandise => res.json(merchandise))
+        .catch(err => res.status(400).json('fetch merchandise ' + req.params.id + ' err: ' + err));
+});
+
+// update a specific merchandise by document ID
+router.route('/update/:id').post((req, res) => {
+   Merchandise.findById(req.params.id)
+       .then(merchandise => {
+           merchandise.Item_ID = req.body.Item_ID;
+           merchandise.Item_Name = req.body.Item_Name;
+           merchandise.Item_Code = req.body.Item_Code;
+           merchandise.Item_Category = req.body.Item_Category;
+           merchandise.Item_Color = req.body.Item_Color;
+           merchandise.Item_Quantity = Number(req.body.Item_Quantity);
+           merchandise.Item_Price = Number(req.body.Item_Price);
+           merchandise.Fabric_Content = req.body.Fabric_Content;
+           merchandise.Description = req.body.Description;
+           merchandise.Special_Offer = req.body.Special_Offer;
+           merchandise.Item_img = req.body.Item_img;
+
+           merchandise.save()
+               .then(() => res.json('merchandise ' + req.params.id + ' has been updated'))
+               .catch(err => res.status(400).json('merchandise update error: ' + err));
+       })
+       .catch(err => res.status(400).json('fetch merchandise ' + req.params.id + ' err: ' + err));
+});
+
+// delete a specific merchandise by document ID
+router.route('/:id').delete((req, res) => {
+    
+});
+
 module.exports = router;
