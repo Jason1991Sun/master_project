728ae7186c57b20e8440be7bea545e2ca66a4924
Jason Sun <jason1991sun@hotmail.com>
2020-09-15 19:38:11 +0930

    update database
    add new item field - short name, new arrive, number of images

--- backend/models/merchandise.model.js
+++ backend/models/merchandise.model.js
@@ -1,24 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 // example to create a model/Schema for cat
-const merchandiseSchema = new Schema({
-    Item_ID: {type: String, required: true},
-    Item_Name: {type: String, required: true},
-    Item_Code: {type: String, required: true},
-    Item_Category: {type: String, required: true},
-    Item_Color: {type: String, required: true},
-    Item_Quantity: {type: Number, required: true},
-    Item_Price: {type: Number, required: true},
-    Fabric_Content: {type: String, required: true},
-    Description: {type: String, required: true},
-    Special_Offer: {type: String, required: true},
-    Item_img: {type: String, required: true}
-}, {
-    timestamps: true
-});
+const merchandiseSchema = new Schema(
+  {
+    Item_ID: { type: String, required: true },
+    Item_Name: { type: String, required: true },
+    Item_Code: { type: String, required: true },
+    Item_Category: { type: String, required: true },
+    Item_Color: { type: String, required: true },
+    Item_Quantity: { type: Number, required: true },
+    Item_Price: { type: Number, required: true },
+    Fabric_Content: { type: String, required: true },
+    Description: { type: String, required: true },
+    Special_Offer: { type: String, required: true },
+    Item_img: { type: String, required: true },
+    Fabric_Content: { type: String, required: true },
+    short_name: { type: String, required: true },
+    new_arrive: { type: String, required: true },
+    no_of_images: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Merchandise = mongoose.model('Merchandise', merchandiseSchema);
+const Merchandise = mongoose.model("Merchandise", merchandiseSchema);
 
 module.exports = Merchandise;
