d3823773ab0d8c9bc55310af5d205e295318e8cf
Jason Sun <jason1991sun@hotmail.com>
2020-09-16 23:22:15 +0930

    home page - update banner
    update home page banner images

--- frontend/src/pages/home/Carousel.js
+++ frontend/src/pages/home/Carousel.js
@@ -5,19 +5,19 @@ export default () => (
   <Carousel autoPlay>
     <div>
       <img alt="" src={process.env.PUBLIC_URL + "/images/banner.jpg"} />
-      <p className="legend">Legend 1</p>
+      <p className="legend"></p>
     </div>
     <div>
       <img alt="" src={process.env.PUBLIC_URL + "/images/banner2.jpg"} />
-      <p className="legend">Legend 2</p>
+      <p className="legend"></p>
     </div>
     <div>
-      <img alt="" src={process.env.PUBLIC_URL + "/images/banner.jpg"} />
-      <p className="legend">Legend 3</p>
+      <img alt="" src={process.env.PUBLIC_URL + "/images/banner3.jpg"} />
+      <p className="legend"></p>
     </div>
     <div>
-      <img alt="" src={process.env.PUBLIC_URL + "/images/banner2.jpg"} />
-      <p className="legend">Legend 4</p>
+      <img alt="" src={process.env.PUBLIC_URL + "/images/banner4.jpg"} />
+      <p className="legend"></p>
     </div>
   </Carousel>
 );
