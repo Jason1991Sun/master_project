b6ae29d507091f9c3fb4c336fd8152c6bf69643b
yuhanj2 <jyhan043@gmail.com>
2020-09-16 14:20:40 +0800

    finished signup page, connected to database succeeded

--- frontend/src/pages/signup/index.js
+++ frontend/src/pages/signup/index.js
@@ -33,19 +33,22 @@ export default class CreateUser extends Component {
   onChangeUsername(e) {
     this.setState({
       username: e.target.value
-    })
+    });
+    console.log('username: ' + e.target.value);
   }
 
   onChangeEmail(e){
     this.setState({
       email: e.target.value
-    })
+    });
+    console.log('email: ' + e.target.value);
   }
 
   onChangePassword(e){
     this.setState({
       password: e.target.value
-    })
+    });
+    console.log('password: ' + e.target.value);
   }
 
   onSubmit(e) {
@@ -69,41 +72,100 @@ export default class CreateUser extends Component {
     })
   }
 
-  render() {
-    return (
-        <div>
-          <h3>Create New User</h3>
-          <form onSubmit={this.onSubmit}>
-            <div className="form-group">
-              <label>Username: </label>
-              <input  type="text"
-                      required
-                      className="form-control"
-                      value={this.state.username}
-                      onChange={this.onChangeUsername}
+  render(){
+    const classes = makeStyles(theme => ({
+      paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+      },
+    }));
+
+    return(
+        <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <br/>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <br/>
+        <form className={classes.form} onSubmit={this.onSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                  name="username"
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  autoFocus
+                  onChange={this.onChangeUsername}
               />
-              <br/>
-              <label>Email: </label>
-              <input  type="text"
-                      required
-                      className="form-control"
-                      value={this.state.email}
-                      onChange={this.onChangeEmail}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  onChange={this.onChangeEmail}
               />
-              <br/>
-              <label>Password: </label>
-              <input  type="text"
-                      required
-                      className="form-control"
-                      value={this.state.password}
-                      onChange={this.onChangePassword}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={this.onChangePassword}
               />
-            </div>
-            <div className="form-group">
-              <input type="submit" value="Create User" className="btn btn-primary" />
-            </div>
-          </form>
-        </div>
-    )
+            </Grid>
+          </Grid>
+          <br/>
+          <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+          <br/>
+    </Container>
+    );
   }
 }
