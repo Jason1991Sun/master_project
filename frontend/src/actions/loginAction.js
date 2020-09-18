daa396882c9496dbc465157ab98e4b7eac21fff9
yuhanj2 <jyhan043@gmail.com>
2020-09-18 20:53:41 +0800

    fixed redux login state at login page

--- frontend/src/actions/loginAction.js
+++ frontend/src/actions/loginAction.js
@@ -1,8 +1,18 @@
-export const login = user => {
-  return {
-      type: 'LOGIN',
-      payload: user
-  };
+import axios from "axios";
+
+export const login = user => async (dispatch) => {
+    const res = await axios.post('http://localhost:5000/users/', user)
+    if (user.password === res.data.password) {
+        console.log('welcome back ' + res.data.username);
+        dispatch({
+            type: 'LOGIN'
+        });
+    } else {
+        console.log('wrong password');
+        dispatch({
+            type: 'LOGOUT'
+        });
+    }
 };
 
 export const logout = () => {
--- frontend/src/reducer/loggedInReducer.js
+++ frontend/src/reducer/loggedInReducer.js
@@ -1,23 +1,7 @@
-import axios from 'axios'
 const loggedInReducer = (state = false, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'LOGIN':
-            const user = action.payload;
-            axios.post('http://localhost:5000/users/', user)
-                .then(res => {
-                    console.log('respond user: ');
-                    console.log(res.data);
-                    if(user.password === res.data.password) {
-                        console.log('welcome back ' + res.data.username);
-                        return true;
-                    }
-                    else{
-                        console.log('wrong password');
-                        return false
-                    }
-                })
-                .catch(err => console.log('validating user failed: ' + err));
-            return false;
+            return true;
         case 'LOGOUT':
             return false;
         default:
